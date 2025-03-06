658f3f2d8fbf99bfd275976ee61f9ff6f7073799
Thiagouh <thiagolorhandasilva12@gmail.com>
2025-03-06 17:27:13 -0300

    refactor: remove index.html from /public for github pages

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/public/index.html");
+  res.sendFile(process.cwd() + "/index.html");
 });
 
 app.get("/api/hello", function (req, res) {
